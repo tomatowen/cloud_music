c21ffd92932c1df0b885d0cb1081960a47ba7e83
zhulu <shaonianzhentan@sina.cn>
2021-07-11 20:42:24 +0800

    修复重定向的问题

--- custom_components/ha_cloud_music/shaonianzhentan.py
+++ custom_components/ha_cloud_music/shaonianzhentan.py
@@ -2,7 +2,10 @@ import aiohttp
 
 # 获取HTTP请求信息
 async def fetch_info(url):
-    headers = {'referer': url}
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36 Edg/91.0.864.48',
+        'referer': url
+    }
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url) as response:
             return {
