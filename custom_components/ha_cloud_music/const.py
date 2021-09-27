52d29b50420651fbd9b5fd109887df35f0e69474
zhulu <shaonianzhentan@sina.cn>
2021-09-27 21:01:14 +0800

    发个版本

--- custom_components/ha_cloud_music/const.py
+++ custom_components/ha_cloud_music/const.py
@@ -1,7 +1,7 @@
 NAME = '云音乐'
 ICON = 'mdi:music'
 DOMAIN = 'ha_cloud_music'
-VERSION = '4.6.4'
+VERSION = '4.7'
 DOMAIN_API = '/' + DOMAIN + '-api'
 WEB_PATH = '/' + DOMAIN + '-web'
 ROOT_PATH = '/' + DOMAIN + '-local/' + VERSION
