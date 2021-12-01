1f054ff58fdc0ed5670985e23c64187a0106c96b
shaonianzhentan <ilovevar@gmail.com>
2021-12-01 23:19:53 +0800

    替换TTS服务

--- custom_components/ha_cloud_music/api_tts.py
+++ custom_components/ha_cloud_music/api_tts.py
@@ -1,78 +1,8 @@
-import os, hashlib, asyncio, threading, time, aiohttp, json, urllib, mutagen
+import os, hashlib, asyncio, threading, time, json, urllib, mutagen, edgeTTS
 from mutagen.mp3 import MP3
 from homeassistant.helpers.network import get_url
 from homeassistant.helpers import template
 from homeassistant.const import (STATE_PLAYING)
-
-# 百度TTS
-IS_PY3 = True
-from urllib.request import urlopen
-from urllib.request import Request
-from urllib.error import URLError
-from urllib.parse import urlencode
-from urllib.parse import quote_plus
-
-API_KEY = '4E1BG9lTnlSeIf1NQFlrSq6h'
-SECRET_KEY = '544ca4657ba8002e3dea3ac2f5fdd241'
-# 发音人选择, 基础音库：0为度小美，1为度小宇，3为度逍遥，4为度丫丫，
-# 精品音库：5为度小娇，103为度米朵，106为度博文，110为度小童，111为度小萌，默认为度小美 
-PER = 4
-# 语速，取值0-15，默认为5中语速
-SPD = 5
-# 音调，取值0-15，默认为5中语调
-PIT = 5
-# 音量，取值0-9，默认为5中音量
-VOL = 5
-# 下载的文件格式, 3：mp3(default) 4： pcm-16k 5： pcm-8k 6. wav
-AUE = 3
-
-FORMATS = {3: "mp3", 4: "pcm", 5: "pcm", 6: "wav"}
-FORMAT = FORMATS[AUE]
-
-CUID = "123456PYTHON"
-
-TTS_URL = 'http://tsn.baidu.com/text2audio'
-class DemoError(Exception):
-    pass
-
-
-"""  TOKEN start """
-
-TOKEN_URL = 'http://openapi.baidu.com/oauth/2.0/token'
-SCOPE = 'audio_tts_post'  # 有此scope表示有tts能力，没有请在网页里勾选
-
-
-def fetch_token():
-    print("fetch token begin")
-    params = {'grant_type': 'client_credentials',
-              'client_id': API_KEY,
-              'client_secret': SECRET_KEY}
-    post_data = urlencode(params)
-    if (IS_PY3):
-        post_data = post_data.encode('utf-8')
-    req = Request(TOKEN_URL, post_data)
-    try:
-        f = urlopen(req, timeout=5)
-        result_str = f.read()
-    except URLError as err:
-        print('token http response http code : ' + str(err.code))
-        result_str = err.read()
-    if (IS_PY3):
-        result_str = result_str.decode()
-
-    print(result_str)
-    result = json.loads(result_str)
-    print(result)
-    if ('access_token' in result.keys() and 'scope' in result.keys()):
-        if not SCOPE in result['scope'].split(' '):
-            raise DemoError('scope is not correct')
-        print('SUCCESS WITH TOKEN: %s ; EXPIRES IN SECONDS: %s' % (result['access_token'], result['expires_in']))
-        return result['access_token']
-    else:
-        raise DemoError('MAYBE API_KEY or SECRET_KEY not correct: access_token or scope not found in token response')
-
-
-"""  TOKEN end """
 
 class ApiTTS():
     def __init__(self, media, cfg):
@@ -135,15 +65,9 @@ class ApiTTS():
         self.log('本地文件路径', ob_name)
         # 文件不存在，则获取下载
         if os.path.isfile(ob_name) == False:
-            token = fetch_token()
-            tex = quote_plus(text)  # 此处TEXT需要两次urlencode
-            print(tex)
-            per = [1,0,3,4][self.tts_mode - 1]
-            params = {'tok': token, 'tex': tex, 'per': per, 'spd': SPD, 'pit': PIT, 'vol': VOL, 'aue': AUE, 'cuid': CUID,
-                    'lan': 'zh', 'ctp': 1}  # lan ctp 固定参数
-
-            data = urlencode(params)
-            urllib.request.urlretrieve(TTS_URL + '?' + data, ob_name)
+            voice = 'zh-CN-XiaoxiaoNeural'
+            asyncio.run(self.write_tts_file(ob_name, voice, text))
+            time.sleep(1)
             # 修改MP3文件属性
             meta = mutagen.File(ob_name, easy=True)
             meta['title'] = text
@@ -177,6 +101,22 @@ class ApiTTS():
             # 恢复音量
             print('恢复音量：%s'%(volume_level))
             self.media._media_player.set_volume_level(volume_level)            
+
+    # 获取TTS文件
+    async def write_tts_file(self, ob_name, voice, message):
+        communicate = edgeTTS.Communicate()
+        lang = 'zh-CN'
+        xml = '<speak version="1.0"' \
+                ' xmlns="http://www.w3.org/2001/10/synthesis"' \
+                ' xmlns:mstts="https://www.w3.org/2001/mstts"' \
+                f' xml:lang="{lang}">' \
+                f'<voice name="{voice}">' \
+                f'{message}</voice></speak>'
+        with open(ob_name, 'wb') as fp:
+            async for i in communicate.run(xml, customspeak=True):
+                if i[2] is not None:
+                    fp.write(i[2])
+        return fp.name
 
     async def speak(self, call):
         try:
